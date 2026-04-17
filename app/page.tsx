8174a664a961d5636292fad12b95138c287975a3
Aser Alemu <asertesfaye@hotmail.com>
2026-04-17 21:15:13 +0300

    done some dashboard components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
+import AppAreaChart from "@/components/AppAreaChart";
+import AppBarChart from "@/components/AppBarChart";
+import AppPieChart from "@/components/AppPieChart";
 import { Button } from "@/components/ui/button";
 import { CirclePlus } from "lucide-react";
 
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Button size="lg" variant="ghost">
-        <CirclePlus />
-        Click Me</Button>
+    <div className="grid grid-cols-1 lg:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppBarChart/></div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg"><AppPieChart /></div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2"><AppAreaChart/></div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
     </div>
   );
 }
